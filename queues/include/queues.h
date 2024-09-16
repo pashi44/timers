b67acc4f3721343e90d4fccf01b8521d17a2f29c
pashi44 <pashireddi@gmail.com>
2024-09-15 22:07:43 -0500

    added  qcb and its definatio s with  pointer  eference

--- queues/include/queues.h
+++ queues/include/queues.h
@@ -2,4 +2,15 @@
 #define __QUEUES__
 #include "tim.h"
 
+//  balking is  called when an  data item is not queued just because of somr consideration
+//  reneging : can a data item cancel its membership in the queue
+
+typedef s_udp_socket queuedata;
+#define QUEUE_DEPTH 10
+
+static uint8_t queueArray[QUEUE_DEPTH * sizeof(queuedata)];
+static StaticQueue_t qobj;
+
+static QueueHandle_t qh;
+
 #endif
